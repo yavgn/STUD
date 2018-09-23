da5eea476b62c211218fabebac3a34c9554fbdde
yavgn <henasam20@yahoo.co.nz>
2018-09-23 22:44:50 +1200

    Save Login skips login activity
    Save Login skips login activity, however it does not save user details, so when you login nothing from previously is saved. We can work on this for sprint 2

--- Studmaster/app/src/main/java/fck2068/example/loginpage/activities/LandingActivity.java
+++ Studmaster/app/src/main/java/fck2068/example/loginpage/activities/LandingActivity.java
@@ -1,23 +1,47 @@
 package fck2068.example.loginpage.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
 import fck2068.example.loginpage.R;
 
 //CLASS to get the extras that we passed
 public class LandingActivity extends AppCompatActivity {
 
     private TextView textViewName;
+    Session session;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        session = new Session(this);
+        if(!session.statusLoggedIn()){
+            logout();
+        }
+        /*
+        logoutButton = (Button)findViewById(R.id.logoutButton);
+        logoutButton.setOnclickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                logout();
+            }
+        });
+        */
 
         textViewName = (TextView) findViewById(R.id.welcome);
         String nameFromIntent = getIntent().getStringExtra("EMAIL");
         textViewName.setText("Welcome " + nameFromIntent);
+
+    }
+
+    private void logout(){
+        session.setLoggedIn(false);
+        finish();
+        startActivity(new Intent(LandingActivity.this,LoginActivity.class));
     }
 }
--- Studmaster/app/src/main/java/fck2068/example/loginpage/activities/LoginActivity.java
+++ Studmaster/app/src/main/java/fck2068/example/loginpage/activities/LoginActivity.java
@@ -1,6 +1,7 @@
 package fck2068.example.loginpage.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
@@ -33,16 +34,23 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private InputValidation inputValidation;
     private DatabaseHelper databaseHelper;
+    private Session session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+        session = new Session(this);
 
         initViews();
         initListeners();
         initObjects();
+
+        if (session.statusLoggedIn()){
+            startActivity(new Intent(LoginActivity.this,LandingActivity.class));
+            finish();
+        }
     }
     private void initViews(){
         nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
@@ -97,7 +105,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent accountsIntent = new Intent(activity, LandingActivity.class);
             accountsIntent.putExtra("EMAIL", textInputEditTextUsername.getText().toString().trim());
             emptyInputEditText();
+            accountsIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            accountsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(accountsIntent);
+            session.setLoggedIn(true);
+
         } else {
             Snackbar.make(nestedScrollView, getString(R.string.error_valid_email_password), Snackbar.LENGTH_LONG).show();
         }
