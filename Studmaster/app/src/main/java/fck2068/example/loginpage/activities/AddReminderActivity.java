e9a20c00da5d218c25038f0e98cbb24fa88a7e6b
DarthKidd <42362465+DarthKidd@users.noreply.github.com>
2018-09-24 01:06:56 +1200

    alert page
    unfinished

---
+++ Studmaster/app/src/main/java/fck2068/example/loginpage/activities/AddReminderActivity.java
@@ -0,0 +1,15 @@
+package fck2068.example.loginpage.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fck2068.example.loginpage.R;
+
+public class AddReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+}
