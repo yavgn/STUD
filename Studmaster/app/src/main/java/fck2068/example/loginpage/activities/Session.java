c54469fafcf2c6aba835ffd5d55f10a2f54fdcc7
yavgn <42362192+yavgn@users.noreply.github.com>
2018-10-09 12:35:36 +1300

    Update Session.java

--- Studmaster/app/src/main/java/fck2068/example/loginpage/activities/Session.java
+++ Studmaster/app/src/main/java/fck2068/example/loginpage/activities/Session.java
@@ -3,22 +3,23 @@ package fck2068.example.loginpage.activities;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+//Session uses shared preferences to stay from logging out
 public class Session {
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
     Context ctx;
-
+    //session takes in context
     public Session(Context ctx){
         this.ctx = ctx;
         prefs = ctx.getSharedPreferences("stud",Context.MODE_PRIVATE);
         editor = prefs.edit();
     }
-
+    //set state to logged in
     public void setLoggedIn(boolean loggedIn){
         editor.putBoolean("loggedIn", loggedIn);
         editor.commit();
     }
-
+    
     public boolean statusLoggedIn(){
         return prefs.getBoolean("loggedIn",false);
     }
