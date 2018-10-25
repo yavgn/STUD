8bed564192b542a81bb2cadfae5916e602fb7f3e
yavgn <henasam20@yahoo.co.nz>
2018-10-25 20:32:12 +1300

    Fixed Maps
    Fixed maps, it now shows picture of campus map

--- Studmaster/app/src/main/java/fck2068/example/loginpage/activities/MapsActivity.java
+++ Studmaster/app/src/main/java/fck2068/example/loginpage/activities/MapsActivity.java
@@ -2,7 +2,6 @@ package fck2068.example.loginpage.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
 
 import fck2068.example.loginpage.R;
 
@@ -11,12 +10,6 @@ public class MapsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps2);
-
-        ImageView mapImage = findViewById(R.id.mapImage);
-        int imageResource = getResources().getIdentifier("@drawable/map_campus.png",null,this.getPackageName());
-        mapImage.setImageResource(imageResource);
-
-
+        setContentView(R.layout.activity_maps);
     }
 }
